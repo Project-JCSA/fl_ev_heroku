01e5f9d0738770699412ee92c2bcdb835e19d9ae
JJ Williams <74847091+jjwilliams11@users.noreply.github.com>
2021-05-12 11:58:12 -0400

    forgot quotes

--- static/js/config.js
+++ static/js/config.js
@@ -1,3 +1,3 @@
 // API key
-const API_KEY = pk.eyJ1Ijoic3Vuc2hpbmVpaCIsImEiOiJja25icnYzYm4xdG5vMm9yaXpiOGZjbnRnIn0.PY7jXS4Xaty_F4V1ez0TQw;
+const API_KEY = "pk.eyJ1Ijoic3Vuc2hpbmVpaCIsImEiOiJja25icnYzYm4xdG5vMm9yaXpiOGZjbnRnIn0.PY7jXS4Xaty_F4V1ez0TQw";
 
